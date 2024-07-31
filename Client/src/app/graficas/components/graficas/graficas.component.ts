bd124634a1b54d5630c6ca2b822046f99c136274
ElihuGmG <martinelihups@gmail.com>
2024-07-30 19:01:20 -0600

    cambios juan 30 de julio
    cambios que hizo juan en el salon

--- Client/src/app/graficas/components/graficas/graficas.component.ts
+++ Client/src/app/graficas/components/graficas/graficas.component.ts
@@ -1,13 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VolverButtonComponent } from '../../../shared/components/volver-button/volver-button.component';
-
+import { ChartModule } from 'primeng/chart';
+import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-graficas',
   standalone: true,
-  imports: [VolverButtonComponent],
+  imports: [VolverButtonComponent, ChartModule, MatListModule],
   templateUrl: './graficas.component.html',
-  styleUrl: './graficas.component.css'
+  styleUrl: './graficas.component.css',
 })
-export class GraficasComponent {
+export class GraficasComponent implements OnInit {
+  options: any;
+  data: any;
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 
+  ngOnInit(): void {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'First Dataset',
+          data: [1,2,3,4,6,7],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          tension: 0.4,
+        },
+        {
+          label: 'Second Dataset',
+          data: [28, 48, 40, 19, 86, 27, 90],
+          fill: false,
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          tension: 0.4,
+        },
+      ],
+    };
+
+    this.options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.6,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+  }
 }
--- Client/src/app/ventilacion/components/ventilacion/ventilacion.component.ts
+++ Client/src/app/ventilacion/components/ventilacion/ventilacion.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { VolverButtonComponent } from '../../../shared/components/volver-button/volver-button.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { InputSwitchModule } from 'primeng/inputswitch';
 @Component({
   selector: 'app-ventilacion',
   standalone: true,
-  imports: [VolverButtonComponent],
+  imports: [
+    VolverButtonComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    InputSwitchModule
+  ],
   templateUrl: './ventilacion.component.html',
-  styleUrl: './ventilacion.component.css'
+  styleUrl: './ventilacion.component.css',
 })
 export class VentilacionComponent {
+
+  ventiladores =['ventilador 1', 'ventilador 2', 'ventilador 3']
+
+}
+
+interface ordenRepo{
+
+   crear():void
+}
+
+class orden{
 
 }
