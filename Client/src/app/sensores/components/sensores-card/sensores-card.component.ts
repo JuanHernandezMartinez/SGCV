6cf5ba42202d7023d626448271e81cab1f8aaccc
ElihuGmG <martinelihups@gmail.com>
2024-09-24 13:30:34 -0600

    añadí la opción de subir imágenes a los sensores
    etapa muy inicial de subir la imagen

--- Client/src/app/sensores/components/sensores-card/sensores-card.component.ts
+++ Client/src/app/sensores/components/sensores-card/sensores-card.component.ts
@@ -1,22 +1,26 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sensores-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './sensores-card.component.html',
   styleUrl: './sensores-card.component.css'
 })
 export class SensoresCardComponent {
-  @Input() public nombre:String="juan"
+  @Input() public nombre: String = "juan";
+  public selectedImage: string | null = null;
 
-
-  public algo():void{
-    console.log("clicked")
+  public handleFileInput(event: any) {
+    const files: FileList = event.target.files;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.selectedImage = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
-
-  
-
 }
