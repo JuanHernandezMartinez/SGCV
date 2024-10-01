261fc25d157315b0ff3c696c0bbdf77cbb260b38
Juan Hernandez <juan.martinez3168@gmail.com>
2024-10-01 16:28:46 -0600

    Diseño completado en las tarjetas de los sensores, falta ajustar responsive para móvil

--- Client/src/app/ventilacion/services/ventilacion.service.ts
+++ Client/src/app/ventilacion/services/ventilacion.service.ts
@@ -1,17 +1,24 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VentilacionService {
+  private url = environment.BASE_ENDPOINT;
   constructor(private http: HttpClient) {}
 
+  public getFans(): Observable<any> {
+    return this.http.get(`${this.url}/sensors`);
+  }
+
+  public getFan(id: number): Observable<any> {
+    return this.http.get(`${this.url}/sensors/${id}`);
+  }
+
   public turnFan(): Observable<void> {
-    return this.http.post<void>(
-      'http://localhost:4000/api/ventilacion/turn',
-      {}
-    );
+    return this.http.post<void>(`${this.url}/ventilacion/turn`, {});
   }
 }
---
+++ Client/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  BASE_ENDPOINT: 'http://localhost:4000/api',
+};
