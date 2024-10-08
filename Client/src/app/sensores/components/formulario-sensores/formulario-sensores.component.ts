21a6181f102292e59293829bc0cfe68b014fcb80
ElihuGmG <martinelihups@gmail.com>
2024-10-08 15:45:40 -0600

    Se genera el componente de formulario de sensores para emmanuel

---
+++ Client/src/app/sensores/components/formulario-sensores/formulario-sensores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-sensores',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-sensores.component.html',
+  styleUrl: './formulario-sensores.component.css'
+})
+export class FormularioSensoresComponent {
+
+}
