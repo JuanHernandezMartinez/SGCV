b73926010bfa7ac41cddc25e5092874dd40ac8e2
ElihuGmG <martinelihups@gmail.com>
2024-10-26 16:52:32 -0600

    Cambios en el home
    se modifico el diseño del home colocando iconos conforme a el menu a seleccionar, tambien se coloco un fondo al mismo para tener la misma apariencia de el login, y se aplico el mismo fondo a la pestaña de sensores

--- Client/src/app/inicio/components/home/home.component.ts
+++ Client/src/app/inicio/components/home/home.component.ts
@@ -1,28 +1,29 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { Router } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
 import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, ButtonModule],
+  imports: [ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
   constructor(private router: Router) {}
 
   salir(): void {
-    console.log('click');
     this.router.navigateByUrl('/');
   }
+
   sensores(): void {
     this.router.navigateByUrl('/sensores');
   }
+
   graficas(): void {
     this.router.navigateByUrl('/graficas');
   }
+
   ventilacion(): void {
     this.router.navigateByUrl('/ventilacion');
   }
