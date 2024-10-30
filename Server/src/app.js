3dcc3b7d7f667ca3e1a5cbfa5018324d962c888c
Juan Hernandez <juan.martinez3168@gmail.com>
2024-10-29 18:42:03 -0600

    mas prueba de sockets

--- Server/src/app.js
+++ Server/src/app.js
@@ -6,6 +6,8 @@ import temperaturasRoutes from "./routes/Temperaturas.routes.js";
 import ventilacionRoutes from "./routes/Ventilacion.routes.js";
 import sensoresRoutes from "./routes/Sensores.routes.js";
 import { conn } from "./db.js";
+import { randomInt } from "crypto";
+
 const app = express();
 const httpServer = http.createServer(app);
 export const io = new Server(httpServer, { cors: "*" });
@@ -19,6 +21,51 @@ app.get("/",(req,res)=>{
     console.log("Entro a la raiz")
     res.send("Hola mundo")
 })
+
+
+io.on("connection", async (socket) => {
+    console.log("Conectado al socket")
+    
+    socket.emit("sexo", "Hola sexoso")
+    socket.broadcast.emit("sexo", "Hola sexoso")
+
+  socket.on("pito", ()=>{
+    socket.emit("sexo1", "Hola sexoso1")
+    socket.broadcast.emit("sexo1", "Hola sexoso1")
+  })
+});
+
+
+
+io.on("temp",  async (socker)=>{
+    try {
+    
+      let mediciones = await obtenerTemperaturas()
+      console.log("Mediciones: ", mediciones)
+      socket.emit("temperaturas", mediciones);
+      socket.broadcast.emit("temperaturas", mediciones);
+    } catch (error) {
+      return "Error al obtener las mediciones"
+    }
+  }
+)
+
+
+
+async function obtenerTemperaturas() {
+  try {
+    console.log("Controller obtener temperaturas");
+    // let tempRequest = await fetch("http://192.168.1.150/temperaturas", { method: "GET" });
+    // let medicion = await tempRequest.json()
+    let medicion = randomInt(5)
+    console.log(medicion)
+    return medicion
+  } catch (error) {
+    console.log("Error en el catch: ", error);
+    return "Error"
+  }
+}
+
 
 // conn();
 export default httpServer;
